24c6ae83d1a7514b2da2b3706e7c5df15cbd8c59
pawida sattayamuk <TangTana@hotmail.com>
2020-10-29 23:59:47 +0700

    1

--- game4/Form1.Designer.cs
+++ game4/Form1.Designer.cs
@@ -41,42 +41,50 @@
             // txtAmmo
             // 
             this.txtAmmo.AutoSize = true;
-            this.txtAmmo.Location = new System.Drawing.Point(41, 33);
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.Location = new System.Drawing.Point(27, 21);
+            this.txtAmmo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txtAmmo.Name = "txtAmmo";
-            this.txtAmmo.Size = new System.Drawing.Size(76, 20);
+            this.txtAmmo.Size = new System.Drawing.Size(99, 24);
             this.txtAmmo.TabIndex = 0;
             this.txtAmmo.Text = "Ammo : 0";
             // 
             // txtScore
             // 
             this.txtScore.AutoSize = true;
-            this.txtScore.Location = new System.Drawing.Point(447, 33);
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(247, 21);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(57, 20);
+            this.txtScore.Size = new System.Drawing.Size(91, 25);
             this.txtScore.TabIndex = 1;
             this.txtScore.Text = "Kills : 0";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(769, 33);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(470, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.Size = new System.Drawing.Size(101, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Health : ";
             // 
             // healthBar
             // 
-            this.healthBar.Location = new System.Drawing.Point(843, 33);
+            this.healthBar.Location = new System.Drawing.Point(564, 21);
+            this.healthBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.healthBar.Name = "healthBar";
-            this.healthBar.Size = new System.Drawing.Size(493, 17);
+            this.healthBar.Size = new System.Drawing.Size(329, 24);
             this.healthBar.TabIndex = 3;
             this.healthBar.Value = 100;
             // 
             // player
             // 
             this.player.Image = global::game4.Properties.Resources.up;
-            this.player.Location = new System.Drawing.Point(616, 371);
+            this.player.Location = new System.Drawing.Point(426, 550);
+            this.player.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.player.Name = "player";
             this.player.Size = new System.Drawing.Size(71, 100);
             this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -91,15 +99,19 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1388, 994);
+            this.ClientSize = new System.Drawing.Size(924, 661);
             this.Controls.Add(this.player);
             this.Controls.Add(this.healthBar);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.txtAmmo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(940, 700);
+            this.MinimumSize = new System.Drawing.Size(940, 678);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
